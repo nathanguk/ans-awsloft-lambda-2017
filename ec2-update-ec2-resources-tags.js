347a3eb81a1a65ab6aa354e64e1eb9d9ae1b5732
nathanguk <nathan.gaskill@ansgroup.co.uk>
2017-09-28 16:57:53 +0100

    Update code to handle instance with no tags

--- ec2-update-ec2-resources-tags.js
+++ ec2-update-ec2-resources-tags.js
@@ -19,7 +19,7 @@ exports.handler = function (event, context, callback) {
         ec2.createTags(tagparrams, function(err, data) {
             if (err) {
                 console.log('Tag Error: ', err);
-                context.fail('Tag Error: ', err );
+                //context.fail('Tag Error: ', err );
             } else {
                 //console.log(data);
                 return data
@@ -46,50 +46,52 @@ exports.handler = function (event, context, callback) {
                     console.log(ec2instance.InstanceId);
 
                     // Get Instance Tags
-                    var ec2instanceTags = ec2instance.Tags;
-                    
-                    // Get Block device mappings
-                    var ec2blockmappings = ec2instance.BlockDeviceMappings;
-
-                    // Loop through block devices in block device mappings
-                    ec2blockmappings.forEach(function(ec2blockdevice) {
-
-                        //Find EBS Volume for block device
-                        var ec2ebsvolume = ec2blockdevice.Ebs;
-                        console.log(ec2ebsvolume.VolumeId);
+                    if(ec2instance.Tags){
+                   
+                        var ec2instanceTags = ec2instance.Tags;
                         
-                        // Applying EC2 Instance Tags to EBS Volume
-                        applytags(ec2instanceTags,[ec2ebsvolume.VolumeId]);
+                        // Get Block device mappings
+                        var ec2blockmappings = ec2instance.BlockDeviceMappings;
 
-                        // Get all Snapshots for Volume
-                        var ec2snapparams = {
-                            Filters: [{Name: "volume-id", Values: [ec2ebsvolume.VolumeId]}]
-                        };
-                        ec2.describeSnapshots(ec2snapparams, function(err, data) {
-                            if (err) {
-                                console.log('Find Snapshots: ', err);
-                                context.fail('Find Snapshots: ', err );
-                            } else {
-                                //Loop through volume Snapshots
-                                var ec2ebssnaps = data.Snapshots
-                                ec2ebssnaps.forEach(function(ec2ebssnap) {
-                                    console.log(ec2ebssnap.SnapshotId);
-                                    // Applying EC2 Instance Tags to Snapshots
-                                    applytags(ec2instanceTags,[ec2ebssnap.SnapshotId]);  
-                                });
+                        // Loop through block devices in block device mappings
+                        ec2blockmappings.forEach(function(ec2blockdevice) {
+
+                            //Find EBS Volume for block device
+                            var ec2ebsvolume = ec2blockdevice.Ebs;
+                            console.log(ec2ebsvolume.VolumeId);
+                            
+                            // Applying EC2 Instance Tags to EBS Volume
+                            applytags(ec2instanceTags,[ec2ebsvolume.VolumeId]);
+
+                            // Get all Snapshots for Volume
+                            var ec2snapparams = {
+                                Filters: [{Name: "volume-id", Values: [ec2ebsvolume.VolumeId]}]
                             };
+                            ec2.describeSnapshots(ec2snapparams, function(err, data) {
+                                if (err) {
+                                    console.log('Find Snapshots: ', err);
+                                    context.fail('Find Snapshots: ', err );
+                                } else {
+                                    //Loop through volume Snapshots
+                                    var ec2ebssnaps = data.Snapshots
+                                    ec2ebssnaps.forEach(function(ec2ebssnap) {
+                                        console.log(ec2ebssnap.SnapshotId);
+                                        // Applying EC2 Instance Tags to Snapshots
+                                        applytags(ec2instanceTags,[ec2ebssnap.SnapshotId]);  
+                                    });
+                                };
+                            });
+                            
                         });
-                        
-                    });
 
-                    // Loop through Network Interfaces on Instance
-                    var ec2enis = ec2instance.NetworkInterfaces;
-                    ec2enis.forEach(function(ec2eni) {
-                        console.log(ec2eni.NetworkInterfaceId);
-                        // Applying EC2 Instance Tags to ENI
-                        applytags(ec2instanceTags,[ec2eni.NetworkInterfaceId]);   
-                    });
-
+                        // Loop through Network Interfaces on Instance
+                        var ec2enis = ec2instance.NetworkInterfaces;
+                        ec2enis.forEach(function(ec2eni) {
+                            console.log(ec2eni.NetworkInterfaceId);
+                            // Applying EC2 Instance Tags to ENI
+                            applytags(ec2instanceTags,[ec2eni.NetworkInterfaceId]);   
+                        });
+                    };
                 });
             });
         }
